5e3e35038831d67a69173ef42d898f85b0c153c0
SWASTIK CHAMOLA <swastik.3814031@gmail.com>
2025-02-06 16:24:02 +0530

    Add files via upload
    Understanding basic flow of and or not logic gates using true and false as input
    The program gives output that are  -> and, or, not.

---
+++ Class and or not logic.py
@@ -0,0 +1,25 @@
+while True:
+    print("enter True and False for understanding logical operators 'and', 'or', 'not'\n")
+    b= input("enter True or False: ")
+    c = input("enter True or False: ")
+    b.lower()
+    c.lower()
+    if b == 'true' or b == 'false':
+        if c == 'true' or c == 'false':
+            print("and operator output", b and c)
+            print("or operator output", b or c)
+            print("not c operator output", not c)
+            print("not b operator output", not b)
+        else:
+            print("invalid input")
+    else:
+        print("invalid input")
+    a = int(input("enter 0 to exit or 1 to continue: "))
+    if a == 0:
+        break
+    else:
+        continue
+
+
+
+
